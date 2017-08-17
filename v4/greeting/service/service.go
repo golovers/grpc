eb601554ec1dd3b1468ba608c7ea1ceebc2e1d23
thanhpham <phamthethanh1088@gmail.com>
2017-08-17 17:29:15 +0700

    implement stream greeting

--- v4/greeting/service/service.go
+++ v4/greeting/service/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	pb "github.com/golovers/grpc/v4/api-golang/gr/greeting/v1"
 	"golang.org/x/net/context"
+	"io"
 )
 
 type Greetings struct{}
@@ -28,4 +29,25 @@ func (gr *Greetings) Say(ctx context.Context, rq *pb.GreetingRequest) (*pb.Greet
 			Message: "We wish you a happy day",
 		},
 	}, nil
+}
+
+func (gr *Greetings) SayAlots(stream pb.Greetings_SayAlotsServer) error {
+	for {
+
+		if req, err := stream.Recv(); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		} else {
+			go func(req *pb.GreetingRequest, str pb.Greetings_SayAlotsServer) {
+				if resp, err := gr.Say(context.Background(), req); err != nil {
+					str.Send(&pb.GreetingResponse{})
+				} else {
+					str.Send(resp)
+				}
+			}(req, stream)
+		}
+	}
+	return nil
 }
